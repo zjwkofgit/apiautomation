49d31fd048abd889d6aaf336770d4db7013d8ae0
重阳 <zhongjw@yunjiglobal.com>
2021-05-12 10:36:31 +0800

    Initial commit

---
+++ src/main/java/com/we/apiautomation/ApiautomationApplication.java
@@ -0,0 +1,13 @@
+package com.we.apiautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiautomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiautomationApplication.class, args);
+    }
+
+}
